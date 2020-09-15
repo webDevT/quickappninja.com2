e3e388e300b34ad97bde850346412c1281509d30
karinadesyatnik <karinadesyatnik27@gmail.com>
2020-09-15 22:27:37 +0300

    optimisation

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,4 @@
 $(function(){
-
-//----start menu-------
 $('.btn__menu').click(function(){
 	$(this).toggleClass('active');
 	$('.header').toggleClass('active');
@@ -8,11 +6,7 @@ $('.btn__menu').click(function(){
 $('.header li').click(function(){
 	$('.header').removeClass('active');
 	$('.btn__menu').removeClass('active');
-
 })
-//----end menu----------
-
-// ------start sticky header------
 $(window).scroll(function() {
 if ($(this).scrollTop() > 1){
 $('.header').addClass("sticky");
@@ -21,46 +15,10 @@ else{
 $('.header').removeClass("sticky");
 }
 });
-//-------end sticky header--------
 
-//-------start img rotate-------
-// setInterval(function() {
-// 	$('.main-screen__img-wrapper').toggleClass('active');
-// }, 5000);
-// start slider
-$('.slider').slick({
-prevArrow: false,
-nextArrow: false,
-slidesToShow: 1,
-dots: true
-});
-// end slider
-
-
+$.lazyLoadXT.onload.addClass = "animated fadeIn";
 
 });
-
-//start captcha//
-
-function scaleCaptcha(elementWidth) {
-  var reCaptchaWidth = 304;
-    var containerWidth = $('.container2').width();
-  if(reCaptchaWidth > containerWidth) {
-    var captchaScale = containerWidth / reCaptchaWidth;
-    $('.g-recaptcha').css({
-      'transform':'scale('+captchaScale+')'
-    });
-  }
-}
-
-$(function() { 
-  scaleCaptcha();
-  $(window).resize( $.throttle( 100, scaleCaptcha ) );
-  
-});
-//end captche 
-
-//start cookies popup
 setTimeout(function(){
   $('.cookies-popup').addClass('active');
 }, 5000);
@@ -68,6 +26,4 @@ setTimeout(function(){
 $('.cookies-popup .button').click(function(){
   $('.cookies-popup').hide();
 })
-//end cookies popup 
-
 
